70a457c8c272996d98e008e5074d12a8943e3bd9
Jason Gary <jason.e.gary@wmich.edu>
2017-03-29 17:37:19 -0400

    Nothing changed

--- src/edu/wmich/cs1120/la5/MapCreatorFromTxt.java
+++ src/edu/wmich/cs1120/la5/MapCreatorFromTxt.java
@@ -5,22 +5,34 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import edu.wmich.cs1120.la3.Area;
+import edu.wmich.cs1120.la3.TerrainScanner;
+
 public class MapCreatorFromTxt implements IMapCreator {
 	private TerrainScanner scanner;
+	private IArea area;
 
 	@Override
 	public void scanTerrain(String fileName, int threshold) throws IOException {
 		String line;
 		String[] values = new String[3];
-
+		Double basicEnergyCost;
+		Double elevation;
+		Double radiation;
+		
 		try {
 			FileReader reader = new FileReader(fileName);
 			BufferedReader buffer = new BufferedReader(reader);
 
-			while ((line = buffer.readLine()) != null) {
-
+			while ((line = buffer.readLine()) != null){
+				values = line.split(" ");
+				basicEnergyCost = Double.parseDouble(values[0]);
+				elevation = Double.parseDouble(values[1]);
+				radiation = Double.parseDouble(values[2]);
+				area = new Area(basicEnergyCost, elevation, radiation, threshold);
+			
 			}
-
+			
 			buffer.close();
 		} catch (FileNotFoundException ex) {
 			System.out.println("Invalid file");
--- src/edu/wmich/cs1120/la5/Substraction.java
+++ src/edu/wmich/cs1120/la5/Substraction.java
@@ -4,7 +4,7 @@ public class Substraction implements IOperation {
 
 	@Override
 	public Integer perform(IExpression left, IExpression right) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
