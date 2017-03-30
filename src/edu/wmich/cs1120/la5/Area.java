61d528e657a1a340d0f9e7b1de5d0bf05a6c5279
Jason Gary <jason.e.gary@wmich.edu>
2017-03-29 23:37:04 -0400

    Fixed map from dat

--- src/edu/wmich/cs1120/la5/Area.java
+++ src/edu/wmich/cs1120/la5/Area.java
@@ -34,61 +34,61 @@ public class Area implements IArea {
 	@Override
 	public double getBasicEnergyCost() {
 		// TODO Auto-generated method stub
-		return 0;
+		return basicEnergyCost;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.wmich.cs1120.la5.IArea#setBasicEnergyCost(double)
 	 */
 	@Override
 	public void setBasicEnergyCost(double basicEnergyCost) {
 		// TODO Auto-generated method stub
-
+		this.basicEnergyCost = basicEnergyCost;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.wmich.cs1120.la5.IArea#getElevation()
 	 */
 	@Override
 	public double getElevation() {
 		// TODO Auto-generated method stub
-		return 0;
+		return elevation;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.wmich.cs1120.la5.IArea#setElevation(double)
 	 */
 	@Override
 	public void setElevation(double elevation) {
 		// TODO Auto-generated method stub
-
+		this.elevation= elevation;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.wmich.cs1120.la5.IArea#getRadiation()
 	 */
 	@Override
 	public double getRadiation() {
 		// TODO Auto-generated method stub
-		return 0;
+		return radiation;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.wmich.cs1120.la5.IArea#setRadiation(double)
 	 */
 	@Override
 	public void setRadiation(double radiation) {
 		// TODO Auto-generated method stub
-
+		this.radiation= radiation;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see edu.wmich.cs1120.la5.IArea#calcConsumedEnergy()
 	 */
 	@Override
 	public double calcConsumedEnergy() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return 0.0;
 	}
 
 }
