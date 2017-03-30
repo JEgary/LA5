8dbb9d856869e2ef387cbe62f34e3aad87902ed7
Jason Gary <jason.e.gary@wmich.edu>
2017-03-29 22:23:49 -0400

    Accidentally moved terrain.txt, but it is back. set up map from dat. and added spavce in main

--- src/edu/wmich/cs1120/la5/LA5Main.java
+++ src/edu/wmich/cs1120/la5/LA5Main.java
@@ -9,10 +9,13 @@ public class LA5Main {
 	public static void main(String[] args) {
 		char userIn = 'y';
 		while( userIn == 'y'||userIn=='Y'){
+		
+		
 		MainWindow.startGUI();
 		
 		userIn=JOptionPane.showInputDialog("Do again? yes or no").charAt(0);
 		}
+		
 
 	}
 
--- src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -8,22 +8,28 @@ public class MapCreatorFromDat implements IMapCreator {
 
 	@Override
 	public void scanTerrain(String fileName, int threshold) throws IOException {
+		Addition add = new Addition();
+		Substraction sub = new Substraction();
+		char biChar = 'o';
 		
 		fileName = "terrain.dat";
 
-		FileInputStream datInputStream = new FileInputStream(fileName);
-		DataInputStream inputFile = new DataInputStream(datInputStream);
-		
-		//inputFile.read();
+		RandomAccessFile datIn = new RandomAccessFile(fileName, "rw");
 		
 		Area[][] datArea = new Area[10][10];
+		Expression val1, val2 = new Expression();
 		
 		for(int row = 0; row<10; row++){
 			for(int col = 0; col<10; col++){
-				datArea[row][col].setElevation(inputFile.readInt());
-				datArea[row][col].setBasicEnergyCost(inputFile.readDouble());
-				datArea[row[col].setRadiation(inputFile.readDouble());
+				datArea[row][col].setBasicEnergyCost(datIn.readDouble());
+				datArea[row][col].setElevation(datIn.readDouble());
+				datArea[row][col].setRadiation(datIn.readDouble());
+				biChar= datIn.readChar();
+				val1.setValue(datIn.readInt());
+				val2.setValue(datIn.readInt());
+				
 			}
+			
 		}
 
 	}
