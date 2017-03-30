239f8571ba51d8e0097e0114e5de8d16188e8586
EdgarCar <edgar.e.cardenas@wmich.edu>
2017-03-29 23:05:39 -0400

    Added documentation to the Expression and RadiationAnalyzer class.

--- src/edu/wmich/cs1120/la5/Expression.java
+++ src/edu/wmich/cs1120/la5/Expression.java
@@ -1,14 +1,26 @@
 package edu.wmich.cs1120.la5;
 
+/**
+ * 
+ * @author Edgar
+ * This class returns the value of the Expression 
+ */
 public class Expression implements IExpression {
 
 	private Integer value;
 
 	@Override
+	/**
+	 * Getter for the integer value
+	 * @return Returns Integer value
+	 */
 	public Integer getValue() {
 		return value;
 	}
-	
+	/**
+	 * Setter for the Integer value
+	 * @param Takes in an integer value
+	 */
 	public void setValue(Integer value){
 		this.value = value;
 		
--- src/edu/wmich/cs1120/la5/RadiationAnalyzer.java
+++ src/edu/wmich/cs1120/la5/RadiationAnalyzer.java
@@ -1,23 +1,41 @@
 package edu.wmich.cs1120.la5;
 
 import java.util.ArrayList;
+/**
+ * 
+ * @author Edgar
+ * This class is the responsible providing the total radiation
+ * of the path
+ *
+ */
 
 public class RadiationAnalyzer implements IRover {
 	private ArrayList<IArea> path;
 	private String analysis;
 
 	@Override
+	/**
+	 * Getter for the IArea ArrayList
+	 * @return Returns the IArea ArrayList
+	 */
 	public ArrayList<IArea> getPath() {
 		return path;
 	}
 
 	@Override
+	/**
+	 * Setter for the IArea ArrayList
+	 * @param Takes in IArea ArrayList
+	 */
 	public void setPath(ArrayList<IArea> path) {
 		this.path = path;
 
 	}
 
 	@Override
+	/**
+	 * calculates the total amount of radiation in the path
+	 */
 	public void analysePath() {
 		double total = 0;
 		for(int i = 0; i < path.size(); i++){
@@ -29,6 +47,10 @@ public class RadiationAnalyzer implements IRover {
 	}
 
 	@Override
+	/**
+	 * Getter for the analysis
+	 * @return Returns String of anlysis
+	 */
 	public String getAnalysis() {
 		return analysis;
 	}
