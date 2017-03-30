7eebba5be7db137286cd43a9ea33944e8d0fbde3
Jason Gary <jason.e.gary@wmich.edu>
2017-03-29 22:47:11 -0400

    moved terrain.dat to the correct location, added terrainScanner asset to map creator class

--- src/edu/wmich/cs1120/la5/LA5Main.java
+++ src/edu/wmich/cs1120/la5/LA5Main.java
@@ -7,16 +7,8 @@ import edu.wmich.cs1120.la5.gui.MainWindow;
 public class LA5Main {
 
 	public static void main(String[] args) {
-		char userIn = 'y';
-		while( userIn == 'y'||userIn=='Y'){
-		
-		
 		MainWindow.startGUI();
 		
-		userIn=JOptionPane.showInputDialog("Do again? yes or no").charAt(0);
 		}
-		
-
-	}
 
 }
--- src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -5,44 +5,52 @@ import java.io.IOException;
 import edu.wmich.cs1120.la5.TerrainScanner; 
 
 public class MapCreatorFromDat implements IMapCreator {
-
+	TerrainScanner tS = new TerrainScanner();
+	
 	@Override
 	public void scanTerrain(String fileName, int threshold) throws IOException {
 		Addition add = new Addition();
 		Substraction sub = new Substraction();
 		char biChar = 'o';
-		
-		fileName = "terrain.dat";
 
 		RandomAccessFile datIn = new RandomAccessFile(fileName, "rw");
 		
 		Area[][] datArea = new Area[10][10];
-		Expression val1, val2 = new Expression();
+		Expression val2 = new Expression();
+		Expression val1 = new Expression();
 		
 		for(int row = 0; row<10; row++){
 			for(int col = 0; col<10; col++){
 				datArea[row][col].setBasicEnergyCost(datIn.readDouble());
 				datArea[row][col].setElevation(datIn.readDouble());
 				datArea[row][col].setRadiation(datIn.readDouble());
+				
 				biChar= datIn.readChar();
 				val1.setValue(datIn.readInt());
 				val2.setValue(datIn.readInt());
 				
+				if(biChar== '+'){
+					datIn.seek(add.perform(val1, val2));
+				}else if(biChar=='-'){
+					datIn.seek(sub.perform(val1, val2));
+				}
 			}
 			
 		}
+		
+		tS.setTerrain(datArea);
 
 	}
 
 	@Override
 	public TerrainScanner getScanner() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return tS;
 	}
 
 	@Override
 	public void setScanner(TerrainScanner scanner) {
-		// TODO Auto-generated method stub
+		tS = scanner;
 
 	}
 
